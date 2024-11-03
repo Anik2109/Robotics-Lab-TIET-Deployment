88022cd8f48ea8883cd5d1304aade03e7caf4a45
Anik Kaushesh <155883621+Anik2109@users.noreply.github.com>
2024-11-03 21:35:30 +0530

    Events Page fixed

--- src/components/Cards/Event Card/event_card.jsx
+++ src/components/Cards/Event Card/event_card.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import '../../Events/events.css'
 
 function Event_Card({ image, title, description }) {
     return ( 
-        <div className="rounded-lg overflow-hidden mx-auto w-[587px] md:w-[500px] sm:w-[350px] xs:w-full" style={{ boxShadow: 'none', backgroundColor: 'transparent' }}>
+        <div 
+            className="rounded-lg overflow-hidden mx-auto w-full sm:max-w-sm md:max-w-md lg:max-w-[620px]"
+            style={{ boxShadow: 'none', backgroundColor: 'transparent' }}
+        >
             <img
                 src={image}
                 alt="Event"
-                className="w-full h-[289px] md:h-[240px] sm:h-[180px] object-cover object-center rounded-t-lg"
+                className="w-full h-[240px] sm:h-[180px] md:h-[240px] lg:h-[289px] object-cover object-center rounded-t-lg"
             />
-            <div className="p-5">
-                <div className="text-[#c61013] text-[22px] md:text-[20px] sm:text-[18px] font-medium font-['Inter']">
+            <div className="p-4 sm:p-4 md:p-5">
+                <div className="text-[#c61013] text-lg sm:text-lg md:text-xl lg:text-2xl font-medium font-['Inter']">
                     {title}
                 </div>
                 <div className="mt-2">
-                    <span className="text-black text-lg md:text-base sm:text-sm font-normal font-['Inter'] whitespace-normal">
+                    <span className="text-black text-base sm:text-base md:text-lg lg:text-xl font-normal font-['Inter'] whitespace-normal">
                         {description}
                     </span>
-                    <span className="text-[#353535] text-lg md:text-base sm:text-sm font-normal font-['Inter'] ml-1">
+                    <span className="text-[#353535] text-base sm:text-base md:text-lg lg:text-xl font-normal font-['Inter'] ml-1">
                         ...Read more
                     </span>
                 </div>
